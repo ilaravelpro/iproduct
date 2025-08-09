8add57c68024d6aa947b4bde35ed7abe1afb276f
Amirhossein Jahani <me@iamir.net>
2025-08-09 06:45:47 +0330

    Update ProductController.php

--- src/app/Http/Controllers/API/v1/ProductController.php
+++ src/app/Http/Controllers/API/v1/ProductController.php
@@ -55,7 +55,66 @@ class ProductController extends \iLaravel\Core\iApp\Http\Controllers\API\ApiCont
                 'title' => _t('order'),
                 'type' => 'number'
             ],
+            [
+                'name' => 'collection_id',
+                'title' => _t('collection'),
+                'type' => 'text'
+            ],
         ];
+        $model->with(['product', 'prices'/*, 'creators', 'sounds', 'electronics'*/]);
+        if ($request->related_id && ($product = imodal('Product')::findByAny($request->related_id))) {
+            $model->where('id', '!=', $product->id);
+            switch ($request->type_data) {
+                case 'collection_products':
+                    $model->whereHas('product', function ($q) use ($product) {
+                        $q->where('collection_id', $product->product->collection_id);
+                    });
+                    break;
+                case 'products_terms':
+                    $model->whereExists(function ($query) use ($product) {
+                        $query->select(\DB::raw(1))
+                            ->from('products_terms')
+                            ->whereColumn('products_terms.product_id', 'products.product_id');
+                        $query->whereIn('products_terms.term_id', $product->terms->pluck('id')->toArray());
+                    });
+                    break;
+                default:
+                    $model
+                        ->where(function ($query) use ($product) {
+                            $query->whereExists(function ($query) use ($product) {
+                                $query->select(\DB::raw(1))
+                                    ->from('products_terms')
+                                    ->whereColumn('products_terms.product_id', 'products.product_id');
+                                $query->whereIn('products_terms.term_id', $product->terms->pluck('id')->toArray());
+                            });
+                            $query->orWhereExists(function ($query) use ($product) {
+                                $query->select(\DB::raw(1))
+                                    ->from('products_creators')
+                                    ->whereColumn('products_creators.product_id', 'products.id');
+                                $query->whereIn('products_creators.creator_id', $product->creators->pluck('id')->toArray());
+                            });
+                        });
+                    break;
+            }
+        }
+        if (is_array($request->years) && count($request->years)) {
+            $start = (new \Morilog\Jalali\Jalalian($request->years[0], 01, 01))->toCarbon()->format('Y-m-d H:i:s');
+            $end = (new \Morilog\Jalali\Jalalian($request->years[1], 01, 01))->toCarbon()->format('Y-m-d H:i:s');
+            $model->whereBetween('first_published_at', [$start, $end]);
+        }
+        if (auth()->check()) {
+            $model->with(['favoritors']);
+            if ($request->is_my_alerts && $request->is_my_alerts == 1) {
+                $model->whereHas('alerts', function ($q) use ($request) {
+                    $q->where('creator_id', auth()->id());
+                });
+            }
+            if ($request->is_my_favorites && $request->is_my_favorites == 1) {
+                $model->whereHas('favoritors', function ($q) use ($request) {
+                    $q->where('users.id', auth()->id());
+                });
+            }
+        }
         return [$filters, [], $operators];
     }
 
@@ -64,11 +123,25 @@ class ProductController extends \iLaravel\Core\iApp\Http\Controllers\API\ApiCont
         switch ($params->type){
             case 'terms':
                 $termModel = imodal('Term');
-                $model->whereHas('terms', function ($query) use ($params, $termModel) {
-                    $query->whereIn('terms.id', array_map(function ($serial) use($termModel) {return $termModel::id($serial); }, $params->value));
-                    return $query;
-                });
+                if ($params->value)
+                    $model->whereHas('terms', function ($query) use ($params, $termModel) {
+                        $query->whereIn('terms.id', array_map(function ($serial) use ($termModel) {
+                            return $termModel::id($serial) ? : (@$termModel::findByAny($serial)->id);
+                        }, $params->value));
+                        return $query;
+                    });
                 $current['terms'] = $filter->value;
+                break;
+            case 'collection_id':
+                $termModel = imodal('ProductCollection');
+                if ($params->value)
+                    $model->whereHas('product', function ($query) use ($params, $termModel) {
+                        $query->whereIn('products.collection_id', array_map(function ($serial) use ($termModel) {
+                            return $termModel::id($serial) ? : (@$termModel::findByAny($serial)->id);
+                        }, $params->value));
+                        return $query;
+                    });
+                $current['collection_id'] = $filter->value;
                 break;
         }
         return $current;
@@ -76,20 +149,22 @@ class ProductController extends \iLaravel\Core\iApp\Http\Controllers\API\ApiCont
 
     public function favorite(Request $request, $record)
     {
-        if (($record = $this->model::findBySerial($record))) {
-            if ($favoritor = $record->favoritors()->where('pivot.user_id', auth()->id())->first()) {
-                $this->statusMessage = 'به لیست علاقه‌مندی های شما افزوده شد.';
-                $record->favoritors()->attach(auth()->id());
-            }else $this->statusMessage = 'از لیست علاقه‌مندی های شما حذف شد.';
+        if ($record = $this->model::findByAny($record)) {
+            if ($record->favoritors()->where('users.id', auth()->id())->first()) {
+                $this->statusMessage = _t("Removed from your wishlist.");
                 $record->favoritors()->detach(auth()->id());
-            return ['data' => []];
+            }else {
+                $this->statusMessage = _t("Added to your wishlist.");
+                $record->favoritors()->attach(auth()->id());
+            }
+            return ['data' => $this->_show($request, $record)];
         }else
-            throw new iException('اطلاعاتی یافت نشد.');
+            throw new iException("No information found.");
     }
 
     public function send_alert(Request $request, $record)
     {
-        if (($record = $this->model::findBySerial($record))) {
+        if ($record = $this->model::findByAny($record)) {
             if (@$record->alerts->count())
                 isms_send("modals.products.alerts.stock", array_filter($record->alerts->map(function ($item) {
                     return @$item->creator->mobile->text;
@@ -97,9 +172,9 @@ class ProductController extends \iLaravel\Core\iApp\Http\Controllers\API\ApiCont
                     'product' => $record->title,
                     'url' => asset($record->type . '/' . ($record->slug?:$record->serial))
                 ]);
-            $this->statusMessage = 'باموفقیت به لیست اعلان موجودی پیامک ارسال گردید.';
+            $this->statusMessage = _t("SMS was successfully sent to the stock notification list.");
             return ['data' => []];
         }else
-            throw new iException('اطلاعاتی یافت نشد.');
+            throw new iException("No information found.");
     }
 }
